248ef4ee63c60a1f5b6dd71e59d4f5f6a03014ff
Thierry Mbelli <mbellimbah@hotmail.com>
2014-04-12 20:49:02 +0200

    initial project structure

---
+++ src/main/java/za/ac/wits/ee/assignment_1/elen7045/dodgegame/App.java
@@ -0,0 +1,13 @@
+package za.ac.wits.ee.assignment_1.elen7045.dodgegame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
